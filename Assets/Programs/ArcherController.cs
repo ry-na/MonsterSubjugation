88b143afd87d8f931e1e12726031b78181fca177
ry-na <ryo.natori0809@gmail.com>
2018-12-19 20:50:16 +0900

    try to move Archer

---
+++ Assets/Programs/ArcherController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcherController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Vector3 tmp = this.transform.position;
+            this.transform.position += new Vector3 (tmp.x + 1, tmp.y, tmp.z); 
+        }
+    }
+}
